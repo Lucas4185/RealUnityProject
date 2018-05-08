107d338ac51f8a7459748a2449690e27543376c0
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2018-05-08 12:26:31 +0200

    blasofjas

---
+++ project/Assets/scripts/collisionScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionScript: MonoBehaviour {
+    void OnCollisionEnter(Collision other)
+    {
+
+        // Destroy everything that leaves the trigger
+        if (other.gameObject.tag == "enemy")
+        {
+            Destroy(other.gameObject);
+            Debug.Log("hit");
+
+        }
+
+    }
+}
