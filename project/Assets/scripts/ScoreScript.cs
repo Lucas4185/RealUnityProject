1732d7cdeed355b56f5acce627bd6dcd641944fa
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2018-05-12 21:10:54 +0200

    first cummit

---
+++ project/Assets/scripts/ScoreScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+
+    private int now = 3;
+    private int score = 0;
+    public Text scoreLabel;
+
+    void Start()
+    {
+       
+        scoreLabel.text = score.ToString(); 
+    }
+
+    void Update()
+    {
+        InvokeRepeating("AdToScore", 1, 1);
+    }
+
+    void AdToScore()
+    {
+        if (now > 0)
+        {
+            score = score + 1;
+            scoreLabel.text = score.ToString();
+        }
+    }
+}
--- project/Assets/scripts/collisionScript.cs
+++ project/Assets/scripts/collisionScript.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class collisionScript: MonoBehaviour {
+
+
     void OnCollisionEnter(Collision other)
     {
 
         // Destroy everything that leaves the trigger
-        if (other.gameObject.tag == "enemy")
+        if (other.gameObject.tag == "bullet")
         {
             Destroy(other.gameObject);
             Debug.Log("hit");
+            Destroy(this.gameObject);
+            
 
         }
 
     }
+    
 }
